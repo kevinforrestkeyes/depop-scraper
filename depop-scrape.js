4ae5e115530485f58753cd3b2d09b022b388a7dd
kevinforrestkeyes <31192191+kevinforrestkeyes@users.noreply.github.com>
2018-09-20 16:25:06 -0700

    now scrolls to bottom of store, gets all items. scrolls until it can see next item before clicking

--- depop-scrape.js
+++ depop-scrape.js
@@ -1,89 +1,127 @@
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: false });
 const fs = require('fs');
+const vo = require('vo');
 
-const start_url = 'https://www.depop.com/__pobrecita__';
+const username = '__pobrecita__';
 const blurb_selector = '.css-1ski12 span span';
 
-const getBlurb = async id => {
-	console.log('getting product');
+const scrapeStore = async (username) => {
+	console.log('now scraping from '+username+'s store');
+	const start_url = "https://www.depop.com/"+username;
 	const nightmare = new Nightmare({ show: true });
 	let product_count = '';
 
 	try {
-		product_count = await nightmare
+		await nightmare
 			.goto(start_url)
-			.wait('[data-css-rabfxd]')
-			.evaluate(() => {
-				return [...document.querySelectorAll('[data-css-rabfxd]')].length;
-			})
-			.then();
+			.wait('[data-css-rabfxd]');
 	} catch(e) {
 		console.log(e);
 	}
 
+	try {
+		var previousHeight, currentHeight = 0;
+		while(previousHeight !== currentHeight) {
+			previousHeight = currentHeight;
+			var currentHeight = await nightmare.evaluate(function() {
+				return document.body.scrollHeight;
+			});
+			await nightmare.scrollTo(currentHeight, 0)
+			.wait(1000);
+		}
+	} catch(e) {
+		console.log(e);
+	}
+
+	try {
+		product_count = await nightmare
+		.evaluate(() => {
+			return [...document.querySelectorAll('[data-css-rabfxd]')].length;
+		})
+		.then();
+	} catch(e) {
+		console.log(e);
+	}
+
+	console.log(product_count+' products to scrape');
 	let full_data = [];
 
-	product_count = 2;
-
 	for(let i = 1; i <= product_count; i++) {
+		console.log('scraping product '+i+'/'+product_count);
+		if(i > 24) {
+			try {
+				let target_visible = false;
+				var currentHeight = 0;
+				while(!target_visible) {
+					var currentHeight = await nightmare.evaluate(function() {
+						return document.body.scrollHeight;
+					})
+					target_visible = await nightmare
+						.exists('li:nth-child('+i+') [data-css-rabfxd]');
+					await nightmare.scrollTo(currentHeight, 0)
+					.wait(1000);
+				}
+			} catch(e) {
+				console.log(e);
+			}
+		}
+
 		try {
 			const result = await nightmare
-				.click('li:nth-child('+i+') [data-css-rabfxd]')
-				.wait('.css-iz6ix8 [data-css-gdcf1g] img')
-				.evaluate(() => {
-					let blurb = [];
-					let fields = [];
-					let values = [];
-					let images = [];
-					if(document.querySelectorAll('.css-1ski12 span span').length > 0) {
-						blurb = [...document.querySelectorAll('.css-1ski12 span span')]
-							.map(el => el.innerText);
-					} else {
-						blurb = [...document.querySelectorAll('.css-1ski12 span')]
-							.map(el => el.innerText);
-					}
-					fields = [...document.querySelectorAll('.css-un4s3n span')]
-						.map(el => el.innerText);
-					values = [...document.querySelectorAll('.css-un4s3n.css-syjz65')]
-						.map(el => el.innerText);
-					images = [...document.querySelectorAll('.css-iz6ix8 [data-css-gdcf1g] img')]
-						.map(el => el.src);
-					return {blurb: blurb.map(line => line.replace('\n','').trim()).filter(line => line !== ''),
-							fields: fields,
-							values: values,
-							images: images}
-				})
-				.then();
-				full_data.push(result);
+			.click('li:nth-child('+i+') [data-css-rabfxd]')
+			.wait('.css-iz6ix8 [data-css-gdcf1g] img')
+			.evaluate(() => {
+				let blurb = [];
+				let fields = [];
+				let values = [];
+				let images = [];
+				if(document.querySelectorAll('.css-1ski12 span span').length > 0) {
+					blurb = [...document.querySelectorAll('.css-1ski12 span span')]
+					.map(el => el.innerText);
+				} else {
+					blurb = [...document.querySelectorAll('.css-1ski12 span')]
+					.map(el => el.innerText);
+				}
+				fields = [...document.querySelectorAll('.css-un4s3n span')]
+				.map(el => el.innerText);
+				values = [...document.querySelectorAll('.css-un4s3n.css-syjz65')]
+				.map(el => el.innerText);
+				images = [...document.querySelectorAll('.css-iz6ix8 [data-css-gdcf1g] img')]
+				.map(el => el.src);
+				return {blurb: blurb.map(line => line.replace('\n','').trim()).filter(line => line !== ''),
+				fields: fields,
+				values: values,
+				images: images}
+			})
+			.then();
+			full_data.push(result);
 		} catch(e) {
 			console.log(e);
 		}
 
 		try {
 			await nightmare
-				.goto(start_url)
-				.wait('[data-css-rabfxd]')
-				.then();
+			.goto(start_url)
+			.wait('[data-css-rabfxd]')
+			.then();
 		} catch(e) {
 			console.log(e);
 		}
 	}
-
 	return JSON.stringify(full_data);
 };
 
-getBlurb(12)
-	.then(a => {
-		fs.writeFile("./scrape_data.json", a, function(err) {
-			if(err) {
-				return console.log(err);
-			}
-
-			console.log("file saved");
-		});
-	})
-	.catch(e => console.error(e));
+scrapeStore(username)
+.then(a => {
+	fs.writeFile("./scrape_data.json", a, function(err) {
+		if(err) {
+			return console.log(err);
+		}
+		return 'file saved';
+	});
+})
+.catch(e => console.error(e));
 
 
 
